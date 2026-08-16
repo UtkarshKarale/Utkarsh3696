9a301abedf481bc2223aaf9d4bab5fee73569722
karaleutkarsh3696 <karaleutkarsh3696@gmail.com>
2026-08-16 20:21:00 +0530

    Ignore build directories

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/blog/lalpari-nostalgia-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Flalpari-nostalgia-website.mdx&astroContentModuleFlag=true")]]);
-		
